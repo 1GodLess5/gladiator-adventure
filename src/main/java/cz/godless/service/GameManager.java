5626520794d3fe4a06d9cf1cdf14b3ac47266c38
1GodLess5 <gacekalbert@seznam.cz>
2024-02-16 07:43:54 +0100

    moved spendHeroAvailablePoints

--- src/main/java/cz/godless/service/GameManager.java
+++ src/main/java/cz/godless/service/GameManager.java
@@ -1,16 +1,20 @@
 package cz.godless.service;
 
 import cz.godless.ability.Ability;
+import cz.godless.ability.HeroAbilityManager;
 import cz.godless.domain.Hero;
 import cz.godless.utility.InputUtils;
+import cz.godless.utility.PrintUtils;
 
 import java.util.Map;
 
 public class GameManager {
     private final Hero hero;
+    private final HeroAbilityManager heroAbilityManager;
 
     public GameManager() {
         this.hero = new Hero("");
+        this.heroAbilityManager = new HeroAbilityManager(this.hero);
     }
 
     public void startGame(){
@@ -22,67 +26,7 @@ public class GameManager {
         System.out.println("Hello " + hero.getName() + ".");
         System.out.println("Let's begin!");
         System.out.println("Your abilities:");
-        for (Map.Entry<Ability, Integer> entry : hero.getAbilities().entrySet()){
-            System.out.print(entry.getKey() + ": " + entry.getValue() + ", ");
-        }
-        System.out.println();
-        spendHeroAvailablePoints(hero);
-    }
-
-    public static void spendHeroAvailablePoints(Hero hero){
-        int availablePoints = hero.getAvailablePoints();
-
-        if (availablePoints == 0){
-            System.out.println("You have no points to spend");
-            return;
-        }
-        while (availablePoints > 0){
-            System.out.println("You have " + availablePoints + " points to spend. Choose wisely.");
-            System.out.println("0 - Explain abilities");
-            System.out.println("1 - Attack");
-            System.out.println("2 - Defence");
-            System.out.println("3 - Dexterity");
-            System.out.println("4 - Skill");
-            System.out.println("5 - Luck");
-            System.out.println("6 - Health");
-
-            final int choice = InputUtils.readInt();
-            Ability ability;
-            switch (choice) {
-                case 0 -> {
-                    for (Ability a: Ability.values()){
-                        System.out.println(a + ": " + a.getDescription());
-                    }
-                    System.out.println();
-                    continue;
-                }
-                case 1 -> ability = Ability.ATTACK;
-                case 2 -> ability = Ability.DEFENCE;
-                case 3 -> ability = Ability.DEXTERITY;
-                case 4 -> ability = Ability.SKILL;
-                case 5 -> ability = Ability.LUCK;
-                case 6 -> ability = Ability.HEALTH;
-                default -> {
-                    System.out.println("Invalid input.");
-                    continue;
-                }
-            }
-            hero.updateAbility(ability, 1);
-            System.out.println("You have upgraded " + ability + ".");
-
-            hero.updateAvailablePoints(-1);
-            if (availablePoints > 1){
-                for (Map.Entry<Ability, Integer> entry : hero.getAbilities().entrySet()){
-                    System.out.print(entry.getKey() + ": " + entry.getValue() + ", ");
-                }
-                System.out.println();
-            }
-            availablePoints--;
-        }
-        System.out.println("You have spent all your available points. Your abilities are:");
-        for (Map.Entry<Ability, Integer> entry : hero.getAbilities().entrySet()){
-            System.out.print(entry.getKey() + ": " + entry.getValue() + ", ");
-        }
-        System.out.println();
+        PrintUtils.printAbilities(hero);
+        this.heroAbilityManager.spendHeroAvailablePoints();
     }
 }
