a8a4e243abb68a8d4c06b4373d7dbf591dba0da2
1GodLess5 <gacekalbert@seznam.cz>
2024-02-16 06:48:09 +0100

    new method spendHeroAvaialblePoints

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
@@ -11,5 +12,32 @@ public class Main {
         final Hero hero = new Hero(name);
         System.out.println("Hello " + hero.getName() + ".");
         System.out.println("Let's begin!");
+        System.out.println("Your abilities:");
+        for (Map.Entry<Ability, Integer> entry : hero.getAbilities().entrySet()){
+            System.out.print(entry.getKey() + ": " + entry.getValue() + ", ");
+        }
+        System.out.println();
+        spendHeroAvailablePoints(hero);
+    }
+
+    public static void spendHeroAvailablePoints(Hero hero){
+        int availablePoints = hero.getAvailablePoints();
+
+        if (availablePoints == 0){
+            System.out.println("You have no points to spend");
+            return;
+        }
+        while (availablePoints > 0){
+            System.out.println("You have " + availablePoints + " to spend. Choose wisely.");
+            System.out.println("0 - Explain abilities");
+            System.out.println("1 - Attack");
+            System.out.println("2 - Defence");
+            System.out.println("3 - Dexterity");
+            System.out.println("4 - Skill");
+            System.out.println("5 - Luck");
+            System.out.println("6 - Health");
+
+            availablePoints--;
+        }
     }
 }
