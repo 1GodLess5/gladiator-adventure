4c7412b7b24a2e7635f779905bb3ef95bc9371b4
1GodLess5 <gacekalbert@seznam.cz>
2024-02-16 07:12:43 +0100

    finished spendHeroAvailablePoints

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,10 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
-    private final static Scanner scanner = new Scanner(System.in);
-
     public static void main(String[] args) {
         System.out.println("Welcome to the Gladiator Adventure");
         System.out.println("Enter your name (this will be used to track your progress):");
-        final String name = scanner.nextLine();
+        final String name = InputUtils.readString();
 
         final Hero hero = new Hero(name);
         System.out.println("Hello " + hero.getName() + ".");
@@ -28,7 +26,7 @@ public class Main {
             return;
         }
         while (availablePoints > 0){
-            System.out.println("You have " + availablePoints + " to spend. Choose wisely.");
+            System.out.println("You have " + availablePoints + " points to spend. Choose wisely.");
             System.out.println("0 - Explain abilities");
             System.out.println("1 - Attack");
             System.out.println("2 - Defence");
@@ -37,7 +35,43 @@ public class Main {
             System.out.println("5 - Luck");
             System.out.println("6 - Health");
 
+            final int choice = InputUtils.readInt();
+            Ability ability;
+            switch (choice) {
+                case 0 -> {
+                    for (Ability a: Ability.values()){
+                        System.out.println(a + ": " + a.getDescription());
+                    }
+                    System.out.println();
+                    continue;
+                }
+                case 1 -> ability = Ability.ATTACK;
+                case 2 -> ability = Ability.DEFENCE;
+                case 3 -> ability = Ability.DEXTERITY;
+                case 4 -> ability = Ability.SKILL;
+                case 5 -> ability = Ability.LUCK;
+                case 6 -> ability = Ability.HEALTH;
+                default -> {
+                    System.out.println("Invalid input.");
+                    continue;
+                }
+            }
+            hero.updateAbility(ability, 1);
+            System.out.println("You have upgraded " + ability + ".");
+
+            hero.updateAvailablePoints(-1);
+            if (availablePoints > 1){
+                for (Map.Entry<Ability, Integer> entry : hero.getAbilities().entrySet()){
+                    System.out.print(entry.getKey() + ": " + entry.getValue() + ", ");
+                }
+                System.out.println();
+            }
             availablePoints--;
         }
+        System.out.println("You have spent all your available points. Your abilities are:");
+        for (Map.Entry<Ability, Integer> entry : hero.getAbilities().entrySet()){
+            System.out.print(entry.getKey() + ": " + entry.getValue() + ", ");
+        }
+        System.out.println();
     }
 }
