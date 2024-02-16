f7f84d14361342238014e453696b0f35e80ede30
1GodLess5 <gacekalbert@seznam.cz>
2024-02-16 07:56:20 +0100

    edited printAbilities()

--- src/main/java/cz/godless/utility/PrintUtils.java
+++ src/main/java/cz/godless/utility/PrintUtils.java
@@ -7,8 +7,14 @@ import java.util.Map;
 
 public class PrintUtils {
     public static void printAbilities(Hero hero){
+        int counter = 0;
         for (Map.Entry<Ability, Integer> entry : hero.getAbilities().entrySet()){
-            System.out.print(entry.getKey() + ": " + entry.getValue() + ", ");
+            if (counter != 5){
+                System.out.print(entry.getKey() + ": " + entry.getValue() + ", ");
+            } else {
+                System.out.print(entry.getKey() + ": " + entry.getValue());
+            }
+            counter++;
         }
         System.out.println();
     }
