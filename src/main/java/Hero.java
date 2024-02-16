eee4d02a1781f09039b87ae9d7534bb324d1f0b0
1GodLess5 <gacekalbert@seznam.cz>
2024-02-16 06:47:09 +0100

    new method getInitialAbilities

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,11 +1,41 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Hero {
-    private String name;
+    private final String name;
+    private Map<Ability, Integer> abilities;
+    private int availablePoints;
 
     public Hero(String name) {
         this.name = name;
+        this.abilities = this.getInitialAbilities();
+        this.availablePoints = 7;
     }
 
     public String getName() {
         return name;
+    }
+
+    public Map<Ability, Integer> getAbilities() {
+        return abilities;
+    }
+
+    public int getAvailablePoints() {
+        return availablePoints;
+    }
+
+    public void setAvailablePoints(int availablePoints) {
+        this.availablePoints = availablePoints;
+    }
+
+    private Map<Ability, Integer> getInitialAbilities() {
+        return new HashMap<>(Map.of(
+                Ability.ATTACK, 1,
+                Ability.DEFENCE, 1,
+                Ability.DEXTERITY, 1,
+                Ability.SKILL, 1,
+                Ability.LUCK, 1,
+                Ability.HEALTH, 50
+        ));
     }
 }
