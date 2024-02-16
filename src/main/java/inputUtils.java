24870f07b5f7276ab4c9d34534373c6b6c871ca1
1GodLess5 <gacekalbert@seznam.cz>
2024-02-16 06:47:39 +0100

    new class, easier input scanning

---
+++ src/main/java/inputUtils.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class inputUtils {
+    private final static Scanner scanner = new Scanner(System.in);
+
+    public static String readString(){
+        return scanner.nextLine();
+    }
+}
