823efc8234a1e6b2358af7d2fa2e7dac0dcc7021
1GodLess5 <gacekalbert@seznam.cz>
2024-02-17 07:14:20 +0100

    new method initGame() -> divided startGame(), new method upgradeAbilitiesCycle()

--- src/main/java/cz/godless/service/GameManager.java
+++ src/main/java/cz/godless/service/GameManager.java
@@ -1,13 +1,10 @@
 package cz.godless.service;
 
-import cz.godless.ability.Ability;
 import cz.godless.ability.HeroAbilityManager;
 import cz.godless.constant.Constant;
 import cz.godless.domain.Hero;
 import cz.godless.utility.InputUtils;
 import cz.godless.utility.PrintUtils;
-
-import java.util.Map;
 
 public class GameManager {
     private final Hero hero;
@@ -20,19 +17,8 @@ public class GameManager {
         this.currentLevel = Constant.INITIAL_LEVEL;
     }
 
-    public void startGame(){
-        System.out.println("Welcome to the Gladiator Adventure");
-        System.out.println("Enter your name (this will be used to track your progress):");
-        final String name = InputUtils.readString();
-
-        this.hero.setName(name);
-        System.out.println("Hello " + hero.getName() + ".");
-        System.out.println("Let's begin!");
-        PrintUtils.printDivider();
-        System.out.println("Your abilities:");
-        PrintUtils.printAbilities(hero);
-        PrintUtils.printDivider();
-        this.heroAbilityManager.spendHeroAvailablePoints();
+    public void startGame() {
+        this.initGame();
 
         while (this.currentLevel <= 5){
             System.out.println("0 - Fight " + "Level " + this.currentLevel);
@@ -48,7 +34,7 @@ public class GameManager {
                     this.currentLevel++;
                 }
                 case 1 -> {
-//                    TODO upgrade abilities
+                    this.upgradeAbilitiesCycle();
                 }
                 case 2 -> {
                     //TODO save game
@@ -67,6 +53,40 @@ public class GameManager {
                 }
                 default -> System.out.println("Invalid input.");
             }
+        }
+
+        System.out.println("You have went through all of the levels. Congratulations!");
+    }
+
+    private void initGame() {
+        System.out.println("Welcome to the Gladiator Adventure");
+        System.out.println("Enter your name (this will be used to track your progress):");
+        final String name = InputUtils.readString();
+
+        this.hero.setName(name);
+        System.out.println("Hello " + hero.getName() + ".");
+        System.out.println("Let's begin!");
+        PrintUtils.printDivider();
+        System.out.println("Your abilities:");
+        PrintUtils.printAbilities(hero);
+        PrintUtils.printDivider();
+        this.heroAbilityManager.spendHeroAvailablePoints();
+    }
+
+    private void upgradeAbilitiesCycle() {
+        System.out.println("Your abilities are: ");
+        PrintUtils.printAbilities(this.hero);
+
+        System.out.println("0 - Go back");
+        System.out.println("1 - Spend points (" + this.hero.getAvailablePoints() + " points to spend)");
+        System.out.println("2 - Remove points");
+
+        final int choice = InputUtils.readInt();
+        switch (choice){
+            case 0 -> {}
+            case 1 -> this.heroAbilityManager.spendHeroAvailablePoints();
+            case 2 -> this.heroAbilityManager.reuseHeroPoints();
+            case 3 -> System.out.println("Invalid input.");
         }
     }
 }
