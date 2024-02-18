fa67aaafa6b233d45cd54b182283186567532972
1GodLess5 <gacekalbert@seznam.cz>
2024-02-18 12:13:16 +0100

    printAbilities() -> edited to GameCharacter instead of Hero

--- src/main/java/cz/godless/utility/PrintUtils.java
+++ src/main/java/cz/godless/utility/PrintUtils.java
@@ -1,14 +1,14 @@
 package cz.godless.utility;
 
 import cz.godless.ability.Ability;
-import cz.godless.domain.Hero;
+import cz.godless.domain.GameCharacter;
 
 import java.util.Map;
 
 public class PrintUtils {
-    public static void printAbilities(Hero hero){
+    public static void printAbilities(GameCharacter character){
         int counter = 0;
-        for (Map.Entry<Ability, Integer> entry : hero.getAbilities().entrySet()){
+        for (Map.Entry<Ability, Integer> entry : character.getAbilities().entrySet()){
             if (counter != 5){
                 System.out.print(entry.getKey() + ": " + entry.getValue() + ", ");
             } else {
