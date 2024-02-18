67d012a88db3360560cf068ed5be7a8f501625fb
1GodLess5 <gacekalbert@seznam.cz>
2024-02-18 13:23:19 +0100

    added constant BATTLE_DELAY_MILLIS

--- src/main/java/cz/godless/constant/Constant.java
+++ src/main/java/cz/godless/constant/Constant.java
@@ -5,4 +5,5 @@ public class Constant {
     public static final int HEALTH_OF_ONE_POINT = 5;
     public static final int INITIAL_LEVEL = 1;
     public static final int CRITICAL_HIT_MULTIPLIER = 2;
+    public static final int BATTLE_DELAY_MILLIS = 500;
 }
