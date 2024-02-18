37e8d7ea34f2d6c1f2c39ed52745752a34f7a82d
1GodLess5 <gacekalbert@seznam.cz>
2024-02-18 11:53:09 +0100

    new abstract class GameCharacter

---
+++ src/main/java/cz/godless/domain/GameCharacter.java
@@ -0,0 +1,23 @@
+package cz.godless.domain;
+
+import cz.godless.ability.Ability;
+
+import java.util.Map;
+
+public abstract class GameCharacter {
+    protected String name;
+    protected Map<Ability, Integer> abilities;
+
+    public GameCharacter(String name, Map<Ability, Integer> abilities) {
+        this.name = name;
+        this.abilities = abilities;
+    }
+
+    public Map<Ability, Integer> getAbilities() {
+        return abilities;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
